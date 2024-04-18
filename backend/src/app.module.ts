95ff6dd3eaa945df09f0745602223b812c56f36a
Khaled Nehad <khalednehad@gmail.com>
2024-04-18 15:55:47 +0200

    inital setup

---
+++ backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { appConfig, databaseConfig } from './config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     ...configService.get('database'),
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [appConfig, databaseConfig],
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/main.ts
@@ -0,0 +1,13 @@
+if (process.env.NODE_ENV === 'development') {
+  require('-alias/register');
+}
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
