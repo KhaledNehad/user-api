be28e97a3ea2d83e2982007d7ee40dd1538d43be
Khaled Nehad <khalednehad@gmail.com>
2024-04-25 10:52:21 +0200

    Auth for the frontend (login/logout/cookies)

--- apps/client/components/forms/SingUpForm.tsx
+++ apps/client/components/forms/SingUpForm.tsx
@@ -33,66 +33,64 @@ function SingUpForm() {
   );
 
   return (
-    <div>
-      <div className="w-full max-w-md">
-        <form action={formAction}>
-          <Card>
-            <CardHeader className="space-y-1">
-              <CardTitle className="text-3xl font-bold">Sign Up</CardTitle>
-              <CardDescription>
-                Enter your details to create a new account
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-4">
-              <div className="space-y-2">
-                <Label htmlFor="username">Username</Label>
-                <Input
-                  id="username"
-                  name="username"
-                  type="text"
-                  placeholder="username"
-                />
-                <ZodErrors error={formState?.ZodError?.username} />
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="name@example.com"
-                />
-                <ZodErrors error={formState?.ZodError?.email} />
-              </div>
-
-              <div className="space-y-2">
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="password"
-                />
-                <ZodErrors error={formState?.ZodError?.password} />
-              </div>
-            </CardContent>
-            <CardFooter className="flex flex-col">
-              <SubmitButton
-                className="w-full"
-                text="Sign Up"
-                loadingText="Loading"
+    <div className="w-full max-w-md">
+      <form action={formAction}>
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-3xl font-bold">Sign Up</CardTitle>
+            <CardDescription>
+              Enter your details to create a new account
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="username">Username</Label>
+              <Input
+                id="username"
+                name="username"
+                type="text"
+                placeholder="username"
               />
-              <ServerErrors error={formState?.message} />
-            </CardFooter>
-          </Card>
-          <div className="mt-4 text-center text-sm">
-            Have an account?
-            <Link className="underline ml-2" href="signin">
-              Sing In
-            </Link>
-          </div>
-        </form>
-      </div>
+              <ZodErrors error={formState?.ZodError?.username} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="name@example.com"
+              />
+              <ZodErrors error={formState?.ZodError?.email} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="password"
+              />
+              <ZodErrors error={formState?.ZodError?.password} />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <SubmitButton
+              className="w-full"
+              text="Sign Up"
+              loadingText="Loading"
+            />
+            <ServerErrors error={formState?.message} />
+          </CardFooter>
+        </Card>
+        <div className="mt-4 text-center text-sm">
+          Have an account?
+          <Link className="underline ml-2" href="signin">
+            Sing In
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
